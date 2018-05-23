a4b4407a1d12bc97b6ca99a1ec952a7bec50fdb1
comptescript <tom.regenet@live.fr>
2018-05-23 14:43:55 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"goland.org/crypto/sha3"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+type Block struct {
+	Index     int
+	Timestamp string
+	Data      string
+	Hash      string
+	PrevHash  string
+}
